be5eb06017ac4c11098d8f76ef42c01b6c3c94c3
phabbs <abdulmumeen.olasode@andela.com>
2018-12-05 18:53:27 +0100

    implement bubble sort

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -1,2 +1,10 @@
 def bubble_sort(number_array):
-    pass
+    array_len = len(number_array)
+    while array_len > 0:
+        for index in range(array_len - 1):
+            if number_array[index] > number_array[index + 1]:
+                number_array[index], number_array[index +
+                                                  1] = number_array[index + 1], number_array[index]
+        array_len -= 1
+
+    return number_array
