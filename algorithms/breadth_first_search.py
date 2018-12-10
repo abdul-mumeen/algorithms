f85dc752ce27d6c04164318c22d54205dd10644a
phabbs <abdulmumeen.olasode@andela.com>
2018-12-10 11:55:40 +0100

    implement breadth first search algorithm

--- algorithms/breadth_first_search.py
+++ algorithms/breadth_first_search.py
@@ -1,2 +1,18 @@
-def breadth_first_search(graph, starting_vertex):
-    pass
+def breadth_first_search(graph_object, starting_vertex):
+    output_sequence = []
+    visited = [False] * len(graph_object.graph)
+
+    queue = []
+
+    queue.append(starting_vertex)
+    visited[starting_vertex] = True
+
+    while queue:
+        vertex = queue.pop(0)
+        output_sequence.append(vertex)
+
+        for index in graph_object.graph[vertex]:
+            if visited[index] == False:
+                queue.append(index)
+                visited[index] = True
+    return output_sequence
