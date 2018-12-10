a2311ce9fc1d7152d94a7be5f80d3c6546027d74
phabbs <abdulmumeen.olasode@andela.com>
2018-12-10 11:39:20 +0100

    add test for breadth first search

---
+++ algorithms/breadth_first_search.py
@@ -0,0 +1,2 @@
+def breadth_first_search(graph, starting_vertex):
+    pass
---
+++ tests/test_breadth_first_search.py
@@ -0,0 +1,12 @@
+import unittest
+from utils import Graph
+from algorithms.algorithms.breadth_first_search import breadth_first_search
+
+
+class TestBreadthFirstSearch(unittest.TestCase):
+
+    def test_breadth_first_search(self):
+        graph = Graph()
+        expected_output_sequence = [2, 0, 3, 1]
+        output = breadth_first_search(graph, 2)
+        self.assertListEqual(output, expected_output_sequence)
